d2e1f5e6a23cd5bc830f99da45188c197ae46de4
Darragh Mcalone <darraghmcalone@hotmail.co.uk>
2020-09-06 10:06:17 +0100

    Add row component and styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
+import Row from './components/Row';
+import requests from './requests';
+
 
 function App() {
+
   return (
     <div className="App">
       <h1>Netflix Clone</h1>
+      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} />
+      <Row title="Trending now" fetchUrl={requests.fetchTrending} />
     </div>
   );
 }
