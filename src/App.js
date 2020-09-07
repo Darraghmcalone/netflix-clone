2c2e8d0df4a468b6ec9e82ba14c228c610e452ce
Darragh Mcalone <darraghmcalone@hotmail.co.uk>
2020-09-07 21:30:58 +0100

    Add Nav Bar

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Row from './components/Row/Row';
 import requests from './requests';
 import Banner from './components/Banner/Banner';
+import Nav from './components/Nav/Nav';
 
 
 function App() {
 
   return (
     <div className="app">
+      <Nav />
       <Banner fetchUrl={requests.fetchNetflixOriginals}/>
       <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} isLargeRow />
       <Row title="Trending now" fetchUrl={requests.fetchTrending} />
