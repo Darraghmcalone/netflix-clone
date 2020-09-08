969c3089e6c9304177ecc00ec3eaada412f7c4b3
Darragh Mcalone <darraghmcalone@hotmail.co.uk>
2020-09-09 00:46:44 +0100

    Add row item component and css file

---
+++ src/components/RowItem/RowItem.js
@@ -0,0 +1,77 @@
+import React, { useState, useEffect } from 'react'
+import { getVideosFromMovie } from '../../axios';
+import Modal from '@material-ui/core/Modal';
+import './RowItem.css'
+
+const baseURL = "https://image.tmdb.org/t/p/original/";
+
+const assignYoutubeTrailer = (videos) => {
+    const YOUTUBE_URLBASE = 'https://www.youtube.com/embed/';
+
+    if (videos.length === 0) {
+        return '';
+    }
+
+    const validVideos = videos.filter(video => video.site.toLowerCase() === 'youtube');
+    const trailers = validVideos.filter(video => video.type.toLowerCase() === 'trailer');
+    const teasers = validVideos.filter(video => video.type.toLowerCase() === 'teaser');
+
+    if (trailers.length !== 0) {
+        return YOUTUBE_URLBASE + trailers[0].key;
+    }
+
+    if (teasers.length !== 0) {
+        return YOUTUBE_URLBASE + teasers[0].key;
+    }
+
+    return '';
+};
+
+function RowItem({ movie, isLargeRow, titleId }) {
+    const [videos, setVideos] = useState('');
+    const [open, setOpen] = useState(false);
+
+    const handleOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    useEffect(() => {
+        getVideosFromMovie(titleId)
+            .then(candidateVideos => setVideos(assignYoutubeTrailer(candidateVideos)));
+    }, [titleId]);
+
+    return (
+        <>
+            <img
+                className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+                key={titleId}
+                src={`${baseURL}${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
+                alt={movie.name || movie.title}
+                onClick={handleOpen}
+            />
+            {videos &&
+                <Modal
+                    open={open}
+                    onClose={handleClose}
+                >
+                    <div className="modal">
+                        <iframe
+                            src={videos}
+                            className="youtubeIframe"
+                            frameBorder="0"
+                            allow="autoplay; encrypted-media"
+                            allowFullScreen
+                            title="video"
+                        />
+                    </div>
+                </Modal>
+            }
+        </>
+    )
+}
+
+export default RowItem
