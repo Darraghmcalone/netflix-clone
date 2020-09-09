46acdcf8d67c50941bae2878a3120e0be5b9ca97
Darragh Mcalone <darraghmcalone@hotmail.co.uk>
2020-09-10 00:48:31 +0100

    Update requests

--- src/components/Row/Row.js
+++ src/components/Row/Row.js
@@ -1,12 +1,11 @@
 import React, { useState, useEffect } from 'react'
 import axios from '../../axios'
 import './Row.css'
-
-const baseURL = "https://image.tmdb.org/t/p/original/";
-
+import RowItem from '../RowItem/RowItem';
 
 export default function Row({ title, fetchUrl, isLargeRow }) {
     const [movies, setMovies] = useState([])
+
     useEffect(() => {
         async function fetchData() {
             const request = await axios.get(fetchUrl)
@@ -15,17 +14,13 @@ export default function Row({ title, fetchUrl, isLargeRow }) {
         }
         fetchData()
     }, [fetchUrl])
-    console.log(movies)
+
     return (
-        <div className='row'>
-            <h1>{title}</h1>
-            <div className='row__posters'>
+        <div className="row">
+            <h2>{title}</h2>
+            <div className="row__posters">
                 {movies.map((movie) => (
-                    <img 
-                    key={movie.id} 
-                    className={`row__poster ${isLargeRow && 'row__posterLarge'} `} 
-                    src={`${baseURL}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} 
-                    alt={movie.title} />
+                    <RowItem key={movie.id} movie={movie} titleId={movie.id} isLargeRow={isLargeRow} />
                 ))}
             </div>
         </div>
