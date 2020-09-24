01fff7b664ca2d0a217fc4bb13d3bec8f3d94abd
Darragh Mcalone <darraghmcalone@hotmail.co.uk>
2020-09-24 12:55:28 +0100

    Remove isLoading

--- src/components/RowItem/RowItem.js
+++ src/components/RowItem/RowItem.js
@@ -1,31 +1,13 @@
-import React, { useState, useEffect } from 'react'
-import useFetch from '../../hooks/useFetch'
+import React from 'react'
 import useModal from '../../hooks/useModal';
 import ModalDetails from '../Modal/ModalDetails'
 import './RowItem.css'
-import requests from '../../api/requests';
-import assignYoutubeTrailer from '../../helper/assignYoutubeTrailer';
 
 const baseURL = "https://image.tmdb.org/t/p/original/";
 
 function RowItem({ movie, isLargeRow, titleId, mediaType }) {
-    const { response, isLoading } = useFetch({
-        method: "get",
-        url: `/${mediaType}/${titleId}/${requests.fetchVideos}`,
-    });
-    const [videos, setVideos] = useState([]);
-
-    useEffect(() => {
-        if (response !== null) {
-            setVideos(assignYoutubeTrailer(response.results));
-        }
-    }, [response]);
 
     const { open, handleOpen, handleClose } = useModal()
-
-    if (isLoading) {
-        return <p>Loading...</p>;
-    }
 
     return (
         <>
@@ -36,7 +18,7 @@ function RowItem({ movie, isLargeRow, titleId, mediaType }) {
                 alt={movie.name || movie.title}
                 onClick={handleOpen}
             />
-            {videos && <ModalDetails videos={videos} open={open} handleClose={handleClose} />}
+            {titleId && <ModalDetails open={open} handleClose={handleClose} mediaType={mediaType} titleId={titleId} />}
         </>
     )
 }
